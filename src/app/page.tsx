b166b420ad37e138469cb3f61da39f18176f775c
Vinibr <soft.vini@gmail.com>
2025-03-12 13:35:01 +0100

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,9 @@
 import { CarouselBackground } from "@/components/CarouselBackground";
 import { PromoForm } from "@/components/PromoForm";
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="h-dvh w-full flex justify-center items-center   ">
       <div className="absolute w-full h-full bg-black/40 z-10"></div>
-      {/* <div className="absolute hidden sm:block  z-10 top-0 left-0 m-5 ">
-        <Image
-          src="/imgs/redbullcom-logo_double-with-text.svg"
-          width={300}
-          height={300}
-          alt="Redbull Logo"
-        />
-      </div> */}
       <div className="absolute w-full h-dvh ">
         <CarouselBackground />
       </div>
